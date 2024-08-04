b2072e0083014579925cd40b0bfdf984cbd99b30
Loïc Vincent-Genod <37535451+VGLoic@users.noreply.github.com>
2024-08-04 15:11:02 +0900

    Allow for soko configuration in hardhat config (#5)
    * set up hardhat config update
    
    * changeset

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,64 @@
+import "hardhat/types/config";
+import { extendConfig } from "hardhat/config";
+import { HardhatConfig, HardhatUserConfig } from "hardhat/types/config";
+import { z } from "zod";
+
 export function add(a: number) {
   return a + 1;
 }
+
+declare module "hardhat/types/config" {
+  export interface HardhatUserConfig {
+    soko?: {
+      storageConfiguration: {
+        type: "aws";
+        awsRegion: string;
+        awsBucketName: string;
+        awsAccessKeyId: string;
+        awsSecretAccessKey: string;
+      };
+    };
+  }
+
+  export interface HardhatConfig {
+    soko?: {
+      storageConfiguration: {
+        type: "aws";
+        awsRegion: string;
+        awsBucketName: string;
+        awsAccessKeyId: string;
+        awsSecretAccessKey: string;
+      };
+    };
+  }
+}
+
+extendConfig(
+  (config: HardhatConfig, userConfig: Readonly<HardhatUserConfig>) => {
+    if (userConfig.soko === undefined) {
+      config.soko = undefined;
+      return;
+    }
+
+    const sokoParsingResult = z
+      .object({
+        storageConfiguration: z.object({
+          type: z.literal("aws"),
+          awsRegion: z.string().min(1),
+          awsBucketName: z.string().min(1),
+          awsAccessKeyId: z.string().min(1),
+          awsSecretAccessKey: z.string().min(1),
+        }),
+      })
+      .safeParse(userConfig.soko);
+
+    if (!sokoParsingResult.success) {
+      console.warn(
+        `Configuration for Soko has been found but seems invalid. Please consult the below errors: \n${sokoParsingResult.error.errors}`,
+      );
+      return;
+    }
+
+    config.soko = sokoParsingResult.data;
+  },
+);
