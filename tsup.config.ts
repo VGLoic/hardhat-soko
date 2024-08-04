8c98e10a396f4bcd624627b90f16bc3045a73cd2
Loïc Vincent-Genod <37535451+VGLoic@users.noreply.github.com>
2024-08-04 14:38:13 +0900

    Set up for initial release (#1)
    * update build instruction, set up for publish
    
    * create initial release
    
    * changeset

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts"],
+  format: ["cjs", "esm"],
+  splitting: false,
   sourcemap: true,
+  dts: true,
   clean: true,
 });
