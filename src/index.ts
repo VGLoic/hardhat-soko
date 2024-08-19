a086332cee119bc177f4cac1f102cef7b0b37cd7
VGLoic <vincent.genod.loic@gmail.com>
2024-08-19 18:10:49 +0200

    add scripts entrypoint

--- src/index.ts
+++ src/index.ts
@@ -10,14 +10,6 @@ import { generateReleasesSummariesAndTypings } from "./scripts/generate-typings"
 import { pushRelease } from "./scripts/push";
 import { generateDiffWithTargetRelease } from "./scripts/diff";
 
-export {
-  pull,
-  pushRelease,
-  generateDiffWithTargetRelease,
-  generateReleasesSummariesAndTypings,
-  retrieveReleasesSummary,
-};
-
 export type SokoHardhatUserConfig = {
   // Local directory in which releases will be pulled
   // Default to `.soko`
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
+  entry: ["src/index.ts", "src/scripts/exports.ts"],
   format: ["cjs", "esm"],
   splitting: false,
   sourcemap: true,
